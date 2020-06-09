044370dd78ce6853443d0ae010fe91792c18b5f0
Tobin Stultiens <tobin.stultiens@gmail.com>
2020-06-09 19:12:51 +0200

    Added that the screen is able to turn black while locked and won't turn black when unlocked

--- i3/customi3lock.sh
+++ i3/customi3lock.sh
@@ -5,4 +5,5 @@ overlay=/usr/share/pixmaps/lockoverlay.png
 scrot -o /tmp/currentworkspace.png
 convert /tmp/currentworkspace.png -blur 0x5 /tmp/currentworkspaceblur.png
 composite -gravity southeast $overlay /tmp/currentworkspaceblur.png /tmp/lockbackground.png
-i3lock -i /tmp/lockbackground.png
+xset s on
+i3lock -i /tmp/lockbackground.png -n; xset s off -dpms
